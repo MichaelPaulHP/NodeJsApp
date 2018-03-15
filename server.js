eb8969cc2a9be8094165a1a9484593e5ba39b094
MichaelPaulHP <michael_h_p_@hotmail.com>
2018-03-14 21:12:27 -0500

    add cors

--- server.js
+++ server.js
@@ -29,6 +29,14 @@ var routes= require("./App/Routes");
 routes.assignRoutes(app);
 
 app.listen(port);
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+ 
+    next();
+});
 app.get("/",function (req, res) {
     res.send("Hola");
 })
