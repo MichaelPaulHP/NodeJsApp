6e49f5f2f4f7a7a7c77b1252d66e383640baaea6
MichaelPaulHP <michael_h_p_@hotmail.com>
2018-05-20 08:46:55 -0500

    go

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 
 // configuration DB ===============================================================
 
-var configDB = require('./Config/database');
+/*var configDB = require('./Config/database');
 mongoose.connect(configDB.url, {
     useMongoClient:true,
     user:configDB.user,
@@ -28,11 +28,11 @@ mongoose.connect(configDB.url, {
         console.log("ERROR MongoDB: "+err.message);
     }
 }); // connect to our database
-
+*/
 //Routes ==============================================================
-var routes= require("./App/Routes");
+/*var routes= require("./App/Routes");
 routes.assignRoutes(app);
-
+*/
 var server = app.listen(port);
 var io = require("socket.io")(server);
 
@@ -46,38 +46,62 @@ app.use((req, res, next) => {
     next();
 });
 
+class User{
+    constructor(id,publicKey){
+        this.id=id;
+        this.publicKey=publicKey;
+    }
+    get id(){
+        return this.id;
+    }
+    get publicKey(){
+        return this.publicKey;
+    }
+
+}
+console.log("GG!");
 var cantUsers=0;
 
-var publicKeys;
+var users={};
 app.get("/",function (req, res) {
     res.send("Hola");
 })
 io.on("connection",  (socket)=> {
-    console.log("Hola desde socketIO ");
-    socket.emit("saludo","HOLA DESDE server")
-    socket.on("rpta", (data)=>{
-       console.log(data);
-   });
+    console.log("new connection ");
+    ++cantUsers;
+    console.log(cantUsers);
    socket.on("newKey",(data)=>{
-        publicKeys = data.publicKey;
-        cantUsers++;
-        
-        socket.emit("getUsers",cantUsers)
+       users.push(new User(socket.id,data));
+
+       socket.emit("getUsers",cantUsers);// envia rla cantidad de usuarios
         if(cantUsers==2){
-            socket.emit("getpublicKey",publicKeys);       
+            // envar la keypublic del Receptor
+
+            socket.emit("getpublicKey",buscarReceptor);       
         }
+
         
    })
    socket.on("newMessage",(data)=>{
-
+       //  
        socket.emit("newMessage",data);
 
    });
-
+   socket.on("disconnect",()=>{
+        --cantUsers;
+        socket.emit("getUsers",cantUsers);
+   });
 
 })
 
-
+var buscarReceptor = function (id){
+    if (users[0].id==id){
+        return users[0].publicKey;
+    }
+    else{
+        return users[1].publicKey;
+    }
+}
 
 
 /*.createServer(function (req, res) {
